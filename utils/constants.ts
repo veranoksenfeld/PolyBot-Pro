b082a5fc0775939b9a53013bb4748d856d1fc3f8
veranoksenfeld <38837266+veranoksenfeld@users.noreply.github.com>
2026-01-13 20:30:51 +0200

    Add files via upload

---
+++ utils/constants.ts
@@ -0,0 +1,15 @@
+
+export const POLYGON_CHAIN_ID = 137;
+export const RPC_URL_FALLBACK = "https://polygon-rpc.com";
+
+// Core Contracts
+export const CTF_EXCHANGE_ADDR = "0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E"; // The main Exchange
+export const CONDITIONAL_TOKENS_ADDR = "0x4D97DCd97E69877AF581718cE6e608d0AF47fC34";
+export const USDC_ADDR = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; // USDC.e
+export const PROXY_WALLET_FACTORY = "0xaB45c5A4B0c941a2F231C04C3f49182e1A254052";
+
+// API Endpoints
+export const GAMMA_API_URL = "https://gamma-api.polymarket.com";
+export const CLOB_API_URL = "https://clob.polymarket.com";
+export const THE_GRAPH_URL = "https://gateway.thegraph.com/api/subgraphs/id/7fu2DWYK93ePfzB24c2wrP94S3x4LGHUrQxphhoEypyY";
+export const THE_GRAPH_URL_LEGACY = "https://api.thegraph.com/subgraphs/name/tokenunion/polymarket-matic";
